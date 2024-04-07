38dfbc7426f4da71caaa4eef1ee7fa7d57acb8b2
Luke Lockhart <llockhart@tuta.io>
2024-04-07 10:45:36 +0100

    split custom config

---
+++ lua/custom/keymaps.lua
@@ -0,0 +1,6 @@
+vim.keymap.set('n', '<leader>e', '<Cmd>Neotree toggle<CR>', { desc = 'Toggle [E]xplorer' })
+
+vim.keymap.set('n', '<leader>\\', '<Cmd>ToggleTerm<CR>', { desc = 'Toggle terminal' })
+vim.keymap.set('n', '<leader>`', '<Cmd>ToggleTerm<CR>', { desc = 'Toggle terminal' })
+
+vim.keymap.set('n', '<leader>p', ":lua require'telescope'.extensions.projects.projects{}<CR>", { desc = 'Projects', noremap = true, silent = true })
