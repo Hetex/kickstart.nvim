ad1ea66c10c0738c6d9c360b3d329d73b091e008
Luke Lockhart <llockhart@tuta.io>
2024-04-07 16:45:45 +0100

    add floating terminal

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -2,5 +2,6 @@ vim.keymap.set('n', '<leader>e', '<Cmd>Neotree toggle<CR>', { desc = 'Toggle [E]
 
 vim.keymap.set('n', '<leader>\\', '<Cmd>ToggleTerm<CR>', { desc = 'Toggle terminal' })
 vim.keymap.set('n', '<leader>`', '<Cmd>ToggleTerm<CR>', { desc = 'Toggle terminal' })
+vim.keymap.set('n', '<leader>t', '<Cmd>ToggleTerm direction=float<CR>', { desc = 'Toggle floating terminal' })
 
 vim.keymap.set('n', '<leader>p', ":lua require'telescope'.extensions.projects.projects{}<CR>", { desc = 'Projects', noremap = true, silent = true })
