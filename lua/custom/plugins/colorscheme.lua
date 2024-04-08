2c50fca961d8fd54b99945c755cb877d396d799e
Luke Lockhart <llockhart@tuta.io>
2024-04-08 22:40:41 +0100

    broke colorschemes

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,10 +1,45 @@
 return {
+  -- tokyonight
+  {
+    'folke/tokyonight.nvim',
+    config = function()
+      require('tokyonight').setup {}
+    end,
+  },
+
+  -- catppuccin
+  {
+    'catppuccin/nvim',
+    config = function()
+      require('catppuccin').setup {}
+    end,
+  },
+
   -- oxocarbon
   {
     'nyoom-engineering/oxocarbon.nvim',
+    config = function()
+      require('oxocarbon').setup {}
+    end,
   },
 
-  -- ondark
+  -- nightfox
+  {
+    'EdenEast/nightfox.nvim',
+    config = function()
+      require('nightfox').setup {}
+    end,
+  },
+
+  -- github
+  {
+    'projekt0n/github-nvim-theme',
+    config = function()
+      require('github-nvim-theme').setup {}
+    end,
+  },
+
+  -- onedark
   {
     'navarasu/onedark.nvim',
     version = '*',
