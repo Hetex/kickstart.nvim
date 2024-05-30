3ac2e3a6cd166778890aa84cd90fcdc85c0da6b4
Luke Lockhart <luke.lockhart@dailypay.com>
2024-05-30 20:35:24 +0100

    update files and add trouble

--- lua/custom/plugins/mini_files.lua
+++ lua/custom/plugins/mini_files.lua
@@ -1,6 +1,13 @@
 return {
   'echasnovski/mini.files',
   version = '*',
+  opts = {
+    mappings = {
+      close = 'q',
+      go_in = 'l',
+      go_in_plus = 'l',
+    },
+  },
   config = function(_, opts)
     require('mini.files').setup(opts)
   end,
